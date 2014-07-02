c621ced138b45d53dcfb368b9ef0464b5b3f464f
Alexander Kushnarev <avkushnarev@gmail.com>
2014-07-02 11:41:28 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='gns-rules-checker',
+      version='0.1',
+      description='Tool to check GNS rules locally',
+      author='Alexander Kushnarev',
+      author_email='avkushnarev@gmail.com',
+      url='https://github.com/yandex-sysmon/gns-rules-checker',
+      packages=['gnsruleschecker'],
+      entry_points={'console_scripts': ['gns_checker = gnsruleschecker.checkrule:main']},
+      requires=['gns'])
