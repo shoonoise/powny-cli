a099c829212e7ce4cda4faab72432fbaaf593949
Alexander Kushnarev <avkushnarev@gmail.com>
2014-07-23 20:07:37 +0400

    oops fix

--- gnscli/settings.py
+++ gnscli/settings.py
@@ -10,9 +10,7 @@ class Settings:
     @classmethod
     def load(cls, ctx, param, file):
         if file:
-            cls.loaded = True
-            config = yaml.load(file)
-            cls.config = config
+            cls.config = yaml.load(file)
         else:
             config = yaml.load(resource_stream(__name__, 'config.yaml'))
             path_to_user_config = os.path.expanduser('~/.config/gnscli/config.yaml')
