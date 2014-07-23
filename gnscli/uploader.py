8873fb847199efaee5043dca033c22ee495f1e0a
Alexander Kushnarev <avkushnarev@gmail.com>
2014-07-23 19:59:04 +0400

    Rename LOG to logger

--- gnscli/uploader.py
+++ gnscli/uploader.py
@@ -7,7 +7,7 @@ from gnscli import gnsapi
 import logging
 from gnscli.settings import Settings
 
-LOG = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class GitCommandError(Exception):
@@ -18,7 +18,7 @@ def _update_head(gns_server: str, path):
     new_head = _execute_git_command(cmd="rev-parse HEAD", extra={'path': path}, err_msg="Can't update HEAD").strip()
     current_head = gnsapi.get_header(gns_server)
     if new_head == current_head:
-        LOG.info("HEAD already updated")
+        logger.info("HEAD already updated")
         return
     gnsapi.set_header(gns_server, new_head)
 
@@ -26,16 +26,16 @@ def _update_head(gns_server: str, path):
 def _execute_git_command(cmd: str, extra: dict, err_msg: str):
     git_warn_exit_code = 1
     full_cmd = 'git --git-dir={path}/.git --work-tree={path} {cmd}'.format(cmd=cmd, **extra)
-    LOG.debug("Execute command: %s", full_cmd)
+    logger.debug("Execute command: %s", full_cmd)
     result = envoy.run(full_cmd)
     out, err, exit_code = result.std_out, result.std_err, result.status_code
     if exit_code > git_warn_exit_code:
         raise GitCommandError(err_msg, err, out, full_cmd)
     elif exit_code == git_warn_exit_code:
-        LOG.warning("%s\n%s", err, out)
+        logger.warning("%s\n%s", err, out)
         return out
     else:
-        LOG.info(out)
+        logger.info(out)
         return out
 
 
@@ -44,31 +44,31 @@ def upload(gns_server, path, message):
     status = _execute_git_command(cmd='status', extra={'path': path}, err_msg="Can't get git status").split('\n')
 
     if 'nothing to commit, working directory clean' in status[3]:
-        LOG.info("There is no changes.")
+        logger.info("There is no changes.")
 
-    LOG.info("Commit current changes...")
+    logger.info("Commit current changes...")
 
     msg = message
 
     _execute_git_command(cmd='commit -a -m {}'.format(msg), extra={'path': path},
                          err_msg="Can't commit your changes")
 
-    LOG.info("Pull changes from rules server...")
+    logger.info("Pull changes from rules server...")
 
     _execute_git_command(cmd='pull --rebase', extra={'path': path},
                          err_msg="Can't pull changes from server rules")
 
-    LOG.info("Sync you changes with rules server...")
+    logger.info("Sync you changes with rules server...")
 
     _execute_git_command(cmd='push', extra={'path': path},
                          err_msg="Can't push your changes")
 
     gns_repos = Settings.config.get("gns_git_remotes")
     for repo in gns_repos:
-        LOG.info("Upload rules to {}...".format(repo))
+        logger.info("Upload rules to {}...".format(repo))
         _execute_git_command(cmd='push {} master'.format(repo), extra={'path': path},
                              err_msg="")
 
     _update_head(gns_server, path)
 
-    LOG.info("You rules uploaded to GNS!")
+    logger.info("You rules uploaded to GNS!")
