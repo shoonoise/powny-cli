506a928b3ff02bb802d420a46b061ea9fb45c31d
Alexander Kushnarev <avkushnarev@gmail.com>
2014-07-31 15:19:04 +0400

    Add tests

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 if __name__ == '__main__':
     setup(name='gnscli',
@@ -12,4 +34,6 @@ if __name__ == '__main__':
           packages=['gnscli'],
           package_data={'gnscli': ['config.yaml']},
           entry_points={'console_scripts': ['gns-cli = gnscli.client:main']},
-          install_requires=['gns', 'raava', 'pyyaml', 'click', 'envoy-beta', 'requests', 'colorlog'])
+          install_requires=['gns', 'raava', 'pyyaml', 'click', 'envoy-beta', 'requests', 'colorlog'],
+          tests_require=['vcrpy', 'pytest-cov'],
+          cmdclass={'test': PyTest})
